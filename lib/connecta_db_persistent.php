e25a2313d03bffca74bb18eb2a0791ead90aae2e
Denis Anfruns <daanfruns@gmail.com>
2021-12-17 17:33:06 +0100

    login

--- lib/connecta_db_persistent.php
+++ lib/connecta_db_persistent.php
@@ -1,11 +1,15 @@
 <?php
-    $cadena_connexio = 'mysql:dbname=cineticsdb;host=localhost:3306';
-    $usuari = 'cinetics';
-    $passwd = 'cinetics';
-    try{
-        //Creem una connexió persistent a BDs
-        $db = new PDO($cadena_connexio, $usuari, $passwd, 
-                        array(PDO::ATTR_PERSISTENT => true));
-    }catch(PDOException $e){
-        echo 'Error amb la BDs: ' . $e->getMessage();
-    }
+    function conexionBBDD(){       
+        $cadena_connexio = 'mysql:dbname=cineticsdb;host=localhost:3306';
+        $usuari = 'cinetics';
+        $passwd = 'cinetics';
+        try{
+            // Creamos la conexión a BBDD
+            $dbCinetics = new PDO($cadena_connexio, $usuari, $passwd, 
+                            array(PDO::ATTR_PERSISTENT => true));
+        }catch(PDOException $e){
+            echo 'Error amb la BDs: ' . $e->getMessage();
+        }
+        return $dbCinetics;
+    }    
+?>
--- lib/transaction.php
+++ lib/transaction.php
@@ -1,24 +1,22 @@
 <?php
     require_once('connecta_db_persistent.php');
-    try{
-        //Iniciem transacció
-        $db->beginTransaction();
-        $sql = "INSERT INTO `users` (mail, username, passHash, userFirstName, userLastName, creationDate, active) 
-                VALUES('$emailPOST', '$userPOST', '$hashPass', 'John', 'Doe', NOW(), 1)";
-        $insert = $db->query($sql);
 
-        if(!$insert){
-            echo '<p> Error: ';
-            echo print_r($db->errorinfo());
-            echo '</p>';
-            //Anulem transacció
-            $db->rollback();
-            echo '<p>Transacció Anulada</p>';
-        }else{
-            $db->commit();
+    function transaction($emailPOST, $userPOST, $hashPass){
+        $dbCinetics=conexionBBDD();
+        try{
+            // Iniciamos transacción
+            $dbCinetics->beginTransaction();
+            $sql = "INSERT INTO `users` (mail, username, passHash, userFirstName, userLastName, creationDate, active) 
+                    VALUES('$emailPOST', '$userPOST', '$hashPass', 'John', 'Doe', NOW(), 1)";
+            $insert = $dbCinetics->query($sql);
+            if($insert){
+                $dbCinetics->commit();
+                echo 'Usuario guardado correctamente';
+            }
+        }catch(PDOException $e){
+            echo 'Error amb la BDs: ' . $e->getMessage();
+            $dbCinetics->rollback();
+            echo 'Transacción abortada';
         }
-        
-    }catch(PDOException $e){
-        echo 'Error amb la BDs: ' . $e->getMessage();
     }
 ?>
