9a48ec3d4631abed15ae9cfea3104d4753d7c6ba
Denis Anfruns <daanfruns@gmail.com>
2022-01-11 23:55:50 +0100

    Verificación & Fronted registro

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  $err = null;
   require "./lib/login.php";
   if($_SERVER["REQUEST_METHOD"] == "POST"){
       
@@ -23,13 +24,6 @@
             header("Location: ./mainPage.php");
             exit;
         }
-      }else{
-        if(empty($_POST['user'])){
-          echo 'El campo user no puede estar vacío <br>';
-        }
-        if(empty($_POST['psw'])){
-          echo 'El campo password no puede estar vacío';
-        }
       }
     }
   }
@@ -42,8 +36,7 @@
     <title>Cinetics</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/custom.css">
-    <script type="text/javascript" src="./js/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="app.js"></script>  
+    <!-- <script type="text/javascript" src="app.js"></script> -->
 </head>
 <body>
   <video autoplay muted loop id="backVideo">
@@ -51,16 +44,21 @@
   </video>
   <div class="col-4 lateral-panel">
     <h1 class="logo">Cinetics</h1>
+    <?php
+    if($err){
+      echo '<p class="text-danger text-center" style="font-weight: bold;">No se ha encontrado el usuario</p>';
+    }
+    ?>
       <form id="login-form" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
           <div class="mb-3">
             <label for="iemail" class="form-label">User or Email</label>
-            <input type="text" class="form-control" id="iemail" name="user" aria-describedby="emailHelp">
+            <input type="text" class="form-control " id="iemail" name="user" aria-describedby="emailHelp">
           </div>
           <div class="mb-3">
             <label for="ipassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="ipassword" name="psw">
+            <input type="password" class="form-control " id="ipassword" name="psw">
           </div>
-          <div class="flex-container">
+          <div class="flex-container" >
             <div class="mb-3 form-check">
               <input type="checkbox" class="form-check-input" id="remember-me">
               <label class="form-check-label" for="remember-me">Remember me</label>
@@ -69,14 +67,24 @@
               <a href="./web/forgotpsw.html">Forgot password?</a>
             </div>
           </div>
-          
           <button type="submit" class="btn submit-button" id="login-submit">Log in</button>
       </form>
+        <?php
+          if($err){
+            if($usuari['user']==""){
+              echo "<script>document.querySelector('.lateral-panel input[type='text']').classList.add('mystyle');</script>";
+            }
+            if($usuari['pass']==""){
 
+            }
+          }
+        ?>
       <div class="sign-up-help">
         <h4>Not yet a memeber? No worry!</h4>
-        <a href="./web/signup.html">Sign up</a>
+        <a href="./lib/signup.php">Sign up</a>
       </div>
 
   </div>
 </body>
+
+<script type="text/javascript" src="./js/index.js"></script>
--- lib/login.php
+++ lib/login.php
@@ -1,15 +1,17 @@
 <?php
     function verificaUsuari($usuari){
+        $bbdd = (object)null;
         require 'conexionDB.php';
         try
         {
             $sql = 'SELECT `passHash` FROM `users` WHERE `username` = :username OR `mail` = :mail';
-            $preparada = $db->prepare($sql);
+            $preparada = $bbdd->prepare($sql);
             $preparada->execute(array(':username' => $usuari['user'], ':mail' => $usuari['user']));
-            echo $preparada->rowCount();
+
             foreach ($preparada as $element) {
                 return password_verify($usuari['pass'], $element['passHash']);
             }
+            
         }catch(PDOException $e)
         {
             echo 'Error amb la BDs: '.$e->getMessage();
