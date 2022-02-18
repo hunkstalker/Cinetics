027d117f3e7946462fbe8d7925cacdbf4074fd1a
valenia <51212398+valenia@users.noreply.github.com>
2022-02-18 11:23:45 +0100

    Head injection
    + added includes for head index and others
    + must change organization in all php which are not index.php

--- mainPage.php
+++ mainPage.php
@@ -12,16 +12,8 @@ if (isset($_COOKIE[session_name()])) {
 }
 ?>
 <!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cinetics</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./css/custom.css">
-    <script type="text/javascript" src="./js/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="app.js"></script>
-</head>
+<!-- TODO: cambiar cuando se relocalizen-->
+<?php include "./includes/indexHead.php" ?>
 <body>
   <video autoplay muted loop id="backVideo">
     <source src="./media/friends.mp4" type="video/mp4">
