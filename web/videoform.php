0d4d791a5ec39dd2dac732ca4bf45d729dd408eb
Denis Anfruns <daanfruns@gmail.com>
2022-02-26 20:29:28 +0100

    CSS de videoform.php Terminado

--- web/videoform.php
+++ web/videoform.php
@@ -13,10 +13,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mida = $_FILES["user_file"]["size"];
 
     // CONFIGURADO EL php.ini EN 100MB
-    if ($mida > (1024 * 1024) * 100) {
+    if ($mida > (1024 * 1024) * 1) {
       $errorFileSize = true;
     } else {
-      $video['title']       = filter_input(INPUT_POST, 'title');
+      // Sólo se ha eliminado el title del diseño, dejo esto así para que no pete en bbdd
+      $video['title'] = "";
       $video['description'] = filter_input(INPUT_POST, 'description');
 
       // Nos aseguramos de que la carpeta de subida de los vídeos existe y sino la crea
@@ -50,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           // SALTARÁN ERRORES POR INTENTAR INSERTAR VALORES QUE YA ESTÁN. LOS OMITIMOS
           $idhashtags = guardarHashtags($video['hashtags']);
           // CONSULTAS INNECESARIAS, NOS LLEVAMOS LAS IDs AL INSERTARLAS
-          // $idvideo    = consultaVideoId($filename);s
+          // $idvideo    = consultaVideoId($filename);
           // $idhashtags = consultaHashtagId($video['hashtags']);
           guardarVideoHashtags($idvideo, $idhashtags);
         }
@@ -76,58 +77,67 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel = 'icon' href = '../media/logo.png' type = 'image/x-icon'>
+  <link rel='icon' href='../media/logo.png' type='image/x-icon'>
   <title>Cinetics</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="../css/custom-two.css">
 </head>
 
 <body>
-  <nav id="navbar" class="navbar navbar-dark bg-dark justify-content-around">
+  <div id="web-content">
+    <video class="d-none d-sm-flex" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+      <source src="../media/waiting.mp4" type="video/mp4">
+    </video>
+    <!-- A partir de aquí el contenido de la web  -->
+
+    <nav id="navbar" class="navbar d-none d-sm-flex navbar-dark bg-dark">
+      <div>
+        <a class="ms-5 display-6 text-white flex-grow-1" href="../index.php">Cinetics</a>
+      </div>
+      <div>
+        <a class="me-5" href="videoform.php"><img type="image" class="btn-nav" src="../media/uploadFile.png"></a>
+        <a class="me-5" href="../lib/logout.php"><img type="image" class="btn-nav" src="../media/user.png"></a>
+      </div>
+    </nav>
+    <!-- Navbar superior tablet/desktop -->
+    
+    <div class="container-fluid p-0">
+      <div class="d-flex align-items-center justify-content-center">
+
+        <div id="central-panel" class="p-4 p-sm-5 mt-3">
+          <form id="signup-form" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+            <div class="head-title mt-5 mb-4">
+              <p class="mb-5 text-center display-4 display-md-6 text-white">Upload your first video!</p>
+            </div>
+            <div class="mb-5">
+              <input id="formFile" name="user_file" class="form-control" type="file">
+              <?php if($errorFileSize){ echo '<p class="text-warning bg-dark text-center" style="font-weight: bold;">Archivo demasiado grande (límite 100 MB)</p>'; } ?>
+            </div>
+            <div class="mb-4">
+              <label for="hashtags" class="form-label mb-0 text-white">Hashtags</label>
+              <input type="text" class="form-control" name="hashtags">
+            </div>
+            <div class="mb-4">
+              <label for="description" class="form-label mb-0 text-white">Description</label>
+              <input type="text" class="form-control" name="description">
+            </div>
+            <div class="mt-4 mb-4">
+              <button type="submit" class="custom-btn">Send</button>
+            </div>
+          </form>
+        </div> <!-- Central-Panel -->
+
+      </div>
+    </div>
+
+  </div>
+
+  <nav id="navbar" class="navbar fixed-bottom d-sm-none navbar-dark bg-dark justify-content-around">
     <a href="../index.php"><img type="image" class="btn-nav" src="../media/home.png"></a>
     <a href="videoform.php"><img type="image" class="btn-nav" src="../media/uploadFile.png"></a>
     <a href="../lib/logout.php"><img type="image" class="btn-nav" src="../media/user.png"></a>
   </nav>
+  <!-- Navbar inferior móvil -->
 
-  <div class="container-fluid p-0">
-    <div class="d-flex align-items-center justify-content-center">
-      
-      <div>
-        <video autoplay muted loop id="back-video">
-          <source src="../media/waiting.mp4" type="video/mp4">
-        </video>
-      </div>
-
-      <div id="central-panel" class="p-4 mt-3">
-        <form id="signup-form" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
-          <div class="head-title mt-5 mb-4">
-            <h2 class="m-0 text-center">Upload your first video!</h2>
-          </div>
-          <div class="mb-4">
-            <label for="file" class="form-label mb-0"></label>
-            <input id="file" name="user_file" class="form-control" type="file">
-            <?php if($errorFileSize){ echo '<p class="text-warning bg-dark text-center" style="font-weight: bold;">Archivo demasiado grande (límite 100 MB)</p>'; } ?>
-          </div>
-          <div class="mb-5">
-            <label for="title" class="form-label mb-0">Title</label>
-            <input type="text" class="form-control" name="title">
-          </div>
-          <div class="mb-5">
-            <label for="hashtags" class="form-label mb-0">Hashtags</label>
-            <input type="text" class="form-control" name="hashtags">
-          </div>
-          <div class="mb-5">
-            <label for="description" class="form-label mb-0">Description</label>
-            <input type="text" class="form-control" name="description">
-          </div>
-          <div class="mt-4 mb-4">
-            <button type="submit" class="btn">Send</button>
-          </div>
-        </form>
-
-      </div>
-    </div>
-  </div>
-    
   <script src="../js/style.js"></script>
 </body>
